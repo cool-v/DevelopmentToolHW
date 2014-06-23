dbd42ead25d4a2b81c853eec74e11a703f292263
cool-v <veltorrent@abv.bg>
2014-06-23 20:04:52 +0300

    Added log4net test

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace StringExtensions
+{
+    class Program
+    {
+        private static readonly ILog log =
+        LogManager.GetLogger(typeof(Program));
+
+        static void Main ()
+        {
+            BasicConfigurator.Configure();
+            log.Info("info");
+            log.Error("error");
+            log.Warn("warn");
+        }
+    }
+}
